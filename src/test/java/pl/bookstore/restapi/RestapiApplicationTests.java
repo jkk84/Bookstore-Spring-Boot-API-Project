9b2a78b748556811fc4ac90ce59c4b2f33193c1d
jkk <jarekkloc84@gmail.com>
2020-11-15 16:24:53 +0100

    initial spring boot files

---
+++ src/test/java/pl/bookstore/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bookstore.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
